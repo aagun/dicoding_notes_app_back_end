9beb442c154af34480556092c39914067f3527f4
aagun <agun@duck.com>
2023-06-03 20:03:37 +0700

    restructure with hapi plugin

---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,82 @@
+const { nanoid } = require("nanoid");
+
+class NotesService {
+  constructor() {
+    this._notes = [];
+  }
+
+  addNote({ title, body, tags }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+      id,
+      title,
+      body,
+      tags,
+      createdAt,
+      updatedAt,
+    };
+
+    this._notes.push(newNote);
+
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+
+    if (isSuccess) {
+      return id;
+    }
+
+    throw new Error("Catatan gagal ditambahkan");
+  }
+
+  getNotes() {
+    return this._notes;
+  }
+
+  getNoteById(id) {
+    const note = this._notes.filter((note) => note.id === id)[0];
+
+    if (note) {
+      return note;
+    }
+
+    throw new Error("Catatan tidak ditemukan");
+  }
+
+  editNoteById(id, { title, body, tags }) {
+    const index = findIndexNoteById(id);
+
+    validateIndexNote(index, "memperbaharui");
+
+    const updatedAt = new Date().toISOString();
+
+    this._notes[index] = {
+      ...this._notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+  }
+
+  deleteNoteById(id) {
+    const index = findIndexNoteById(id);
+
+    validateIndexNote(index, "menghapus");
+
+    this._notes.splice(index, 1);
+  }
+
+  findIndexNoteById(id) {
+    return this._notes.findIndex((note) => note.id === id);
+  }
+
+  validateIndexNote(index, message) {
+    if (index < 0) {
+      throw new Error(`Gagal ${message} catatan. Id ${id} tidak ditemukan`);
+    }
+  }
+}
+
+module.exports = NotesService;
